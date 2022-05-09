20e9c782353e056d80b73f980284d631a3dd0354
Simona Prokic <167simonap@gmail.com>
2022-05-09 15:59:22 +0200

    Separe table for heuristics

--- src/app/modules/annotation-schema/annotation-schema.component.ts
+++ src/app/modules/annotation-schema/annotation-schema.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { MatTable, MatTableDataSource } from "@angular/material/table";
 import { ToastrService } from "ngx-toastr";
 import { ConfirmDialogComponent } from "../data-set/dialogs/confirm-dialog/confirm-dialog.component";
 import { CodeSmellDefinitionService } from "./services/code-smell-definition.service";
 import { AddCodeSmellDialogComponent } from "./dialogs/add-code-smell-dialog/add-code-smell-dialog.component";
-import { HeuristicsDialogComponent } from "./dialogs/heuristics-dialog/heuristics-dialog.component";
 import { SeverityRangeDialogComponent } from "./dialogs/severity-range-dialog/severity-range-dialog.component";
 import { SeverityValuesDialogComponent } from "./dialogs/severity-values-dialog/severity-values-dialog.component";
 import { UpdateCodeSmellDialogComponent } from "./dialogs/update-code-smell-dialog/update-code-smell-dialog.component";
 import { CodeSmellDefinition } from "./model/code-smell-definition/code-smell-definition.model";
 import { numberToSnippetType, SnippetType } from "./model/enums/enums.model";
+import { AddHeuristicDialogComponent } from "./dialogs/add-heuristic-dialog/add-heuristic-dialog.component";
+import { Heuristic } from "./model/heuristic/heuristic.model";
+import { UpdateHeuristicDialogComponent } from "./dialogs/update-heuristic-dialog/update-heuristic-dialog.component";
 
 
 @Component({
@@ -22,13 +24,16 @@ import { numberToSnippetType, SnippetType } from "./model/enums/enums.model";
 export class AnnotationSchemaComponent implements OnInit {
 
   public codeSmellDefinitions: CodeSmellDefinition[] = [];
-  public codeSmellsDisplayedColumns = ['name', 'description', 'snippetType', 'heuristics', 'severityRange', 'severityValues', 'edit', 'delete'];
+  public codeSmellsDisplayedColumns = ['name', 'description', 'snippetType', 'severityRange', 'severityValues', 'edit', 'delete'];
   public codeSmellsDataSource = new MatTableDataSource<CodeSmellDefinition>();
   public selectedSnippetType: SnippetType | null = null;
   public snippetTypes: string[] = Object.keys(SnippetType);
+  public chosenCodeSmellDefinition: CodeSmellDefinition;
+  public heuristics: Heuristic[] = [];
+  public heuristicsDisplayedColumns = ['name', 'description', 'edit', 'delete'];
+  public heuristicsDataSource = new MatTableDataSource<Heuristic>();
 
-  @ViewChild(MatTable) codeSmellsTable: MatTable<CodeSmellDefinition>;
-
+  @ViewChildren(MatTable) public table : QueryList<MatTable<any>>;
   constructor(private codeSmellDefinitionService: CodeSmellDefinitionService,
     public dialog: MatDialog, private toastr: ToastrService) {}
 
@@ -46,20 +51,13 @@ export class AnnotationSchemaComponent implements OnInit {
     dialogRef.afterClosed().subscribe(createdCodeSmell => {
       if (createdCodeSmell == '') return;
       this.codeSmellsDataSource.data.push(numberToSnippetType(createdCodeSmell));
-      this.codeSmellsTable.renderRows();
+      this.table.first.renderRows();
     });
   }
 
   public searchCodeSmells(event: Event): void {
     const input = (event.target as HTMLInputElement).value;
     this.codeSmellsDataSource.data = this.codeSmellDefinitions.filter(c => c.name.toLowerCase().includes(input.toLowerCase()));
-  }
-
-  public showHeuristics(codeSmellDefinition: CodeSmellDefinition): void {
-    let dialogRef = this.dialog.open(HeuristicsDialogComponent, {
-      data: codeSmellDefinition,
-    });
-    dialogRef.updateSize('50%');
   }
 
   public showSeverityRange(codeSmellDefinition: CodeSmellDefinition): void {
@@ -106,5 +104,47 @@ export class AnnotationSchemaComponent implements OnInit {
 
   private snippetTypeSelected(): boolean {
     return this.selectedSnippetType == SnippetType.Class || this.selectedSnippetType == SnippetType.Function;
+  }
+
+  public chooseCodeSmell(chosenSmell: CodeSmellDefinition) {
+    this.chosenCodeSmellDefinition = chosenSmell;
+    this.codeSmellDefinitionService.getHeuristicsForCodeSmell(this.chosenCodeSmellDefinition.id).subscribe(res => {
+      this.heuristics = res;
+      this.heuristicsDataSource.data =  res;
+    })
+  }
+
+  public searchHeuristics(event: Event): void {
+    const input = (event.target as HTMLInputElement).value;
+    this.heuristicsDataSource.data = this.heuristics.filter(h => h.name.toLowerCase().includes(input.toLowerCase()));
+  }
+
+  public addHeuristic(): void {
+    let dialogRef = this.dialog.open(AddHeuristicDialogComponent);
+    dialogRef.updateSize('20%');
+    dialogRef.afterClosed().subscribe(createdHeuristic => {
+      if (createdHeuristic == '') return;
+      this.heuristicsDataSource.data.push(createdHeuristic);
+      this.table.last.renderRows();
+      this.chosenCodeSmellDefinition = numberToSnippetType(this.chosenCodeSmellDefinition!);
+      this.codeSmellDefinitionService.addHeuristicToCodeSmell(this.chosenCodeSmellDefinition?.id!, createdHeuristic)
+      .subscribe();
+    });
+  }
+
+  public removeHeuristic(heuristic: Heuristic): void {
+    this.heuristicsDataSource.data.splice(this.heuristicsDataSource.data.findIndex(h => h.id == heuristic.id), 1);
+    this.codeSmellDefinitionService.removeHeuristicFromCodeSmell(this.chosenCodeSmellDefinition?.id!, heuristic.id)
+    .subscribe(res => this.table.last.renderRows());
+  }
+
+  public updateHeuristic(heuristic: Heuristic): void {
+    let dialogRef = this.dialog.open(UpdateHeuristicDialogComponent, {
+      data: [this.chosenCodeSmellDefinition, heuristic]
+    });
+    dialogRef.updateSize('20%');
+    dialogRef.afterClosed().subscribe((updated: Heuristic) => {
+      if (updated) this.toastr.success('Updated heuristic ' + updated.name);
+    });
   }
 }
--- src/app/modules/annotation-schema/annotation-schema.module.ts
+++ src/app/modules/annotation-schema/annotation-schema.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AnnotationSchemaComponent } from './annotation-schema.component';
-import { HeuristicsDialogComponent } from './dialogs/heuristics-dialog/heuristics-dialog.component';
 import { SeverityRangeDialogComponent } from './dialogs/severity-range-dialog/severity-range-dialog.component';
 import { SeverityValuesDialogComponent } from './dialogs/severity-values-dialog/severity-values-dialog.component';
 import { UpdateCodeSmellDialogComponent } from './dialogs/update-code-smell-dialog/update-code-smell-dialog.component';
@@ -15,7 +14,6 @@ import { UpdateHeuristicDialogComponent } from './dialogs/update-heuristic-dialo
 @NgModule({
   declarations: [
     AnnotationSchemaComponent,
-    HeuristicsDialogComponent,
     SeverityRangeDialogComponent,
     SeverityValuesDialogComponent,
     UpdateCodeSmellDialogComponent,
