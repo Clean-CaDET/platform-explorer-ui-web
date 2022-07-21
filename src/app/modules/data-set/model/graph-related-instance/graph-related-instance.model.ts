8ad693947217c7674b6c4d297776d47efc79ec70
Simona Prokic <167simonap@gmail.com>
2022-07-21 16:25:39 +0200

    Add RelationType to GraphRelatedInstance

--- src/app/modules/data-set/model/graph-related-instance/graph-related-instance.model.ts
+++ src/app/modules/data-set/model/graph-related-instance/graph-related-instance.model.ts
@@ -1,15 +1,38 @@
-import { CouplingType } from "../enums/enums.model";
+import { CouplingType, RelationType } from "../enums/enums.model";
 
 export class GraphRelatedInstance {
     id: number = 0;
     codeSnippetId: string = '';
+    relationType: string = '';
     couplingTypeAndStrength: Map<CouplingType, number> = new Map();
 
     constructor(obj?: any) {
         if (obj) {
             this.id = obj.id;
             this.codeSnippetId = obj.codeSnippetId;
+            this.setRelationType(obj.relationType);
             this.couplingTypeAndStrength = obj.couplingTypeAndStrength;
+        }
+    }
+
+    private setRelationType(type: string): void {
+        switch(+type) {
+            case 0: {
+                this.relationType = RelationType.Referenced;
+                break;
+            }
+            case 1: { 
+                this.relationType = RelationType.References;
+                break; 
+            } 
+            case 2: { 
+                this.relationType = RelationType.Parent;
+                break; 
+            } 
+            default: {
+                this.relationType = type as RelationType;
+                break;
+            }
         }
     }
 }
