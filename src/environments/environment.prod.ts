fc2814d13b882a8186f3d649b7a762c4796a1e9a
simsimkic <167simonap@gmail.com>
2026-01-20 17:50:19 +0100

    Add Dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiHost: 'http://localhost:8000/api/',
+  apiHost: 'http://localhost:5000/api/', //dockerized BE
+  //apiHost: 'http://localhost:8000/api/' //local BE
 };
